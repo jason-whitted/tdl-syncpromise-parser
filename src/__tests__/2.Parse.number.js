501a72e3b53f32a6fed829b836e5515cefb4a20d
Jason Whitted <jwhitted@ent.com>
2017-08-18 14:50:59 -0600

    Add basic Parse.number tests

--- src/__tests__/2.Parse.number.js
+++ src/__tests__/2.Parse.number.js
@@ -10,22 +10,112 @@ describe('Parse.number', () => {
     expect(parser).toBeA(Function);
   });
 
-  it('() should return a function', () => {
-    expect(parser()).toBeA(Function);
+  describe('()', () => {
+    it('() should return a function', () => {
+      expect(parser()).toBeA(Function);
+    });
+
+    it('()() should return a SyncPromise', () => {
+      const promise = parser()();
+      expect(promise).toBeA(SyncPromise);
+    });
+
+    it('()().value.resolved() should return NaN', () => {
+      const expected = parser()().value.resolved();
+      expect(isNaN(expected)).toBe(true);
+    });
+
+    it('()("-123").value.resolved() should return -123', () => {
+      const expected = parser()("123").value.resolved();
+      expect(expected).toBe(-123);
+    });
+
+    it('()("xyz").value.rejected() should return "Invalid"', () => {
+      const expected = parser()('xyz').value.rejected();
+      expect(expected).toBe('Invalid');
+    });
+
+    it('()("$12,345.67").value.resolved() should return 12345.67', () => {
+      const expected = parser()("$12,345.67").value.resolved();
+      expect(expected).toBe(12345.67);
+    });
   });
 
-  it('()() should return a SyncPromise', () => {
-    const promise = parser()();
-    expect(promise).toBeA(SyncPromise);
+  describe('({ required: true })', () => {
+    it('().value.rejected() should return "Required"', () => {
+      const expected = parser({ required: true })().value.rejected();
+      expect(expected).toBe('Required');
+    });
+
+    it('(".123").value.resolved() should return 0.123', () => {
+      const expected = parser({ required: true })('.123').value.resolved();
+      expect(expected).toBe(0.123);
+    });
+
+    it('("abc").value.rejected() should return "Invalid"', () => {
+      const expected = parser({ required: true })("abc").value.rejected();
+      expect(expected).toBe('Invalid');
+    });
   });
 
-  it('()("123").value() should return 123', () => {
-    const expected = parser()("123").value();
-    expect(expected).toBe(123);
+  describe('({ min: 0 })', () => {
+    it('("-0.001").value.rejected() should return "Too Low"', () => {
+      const expected = parser({ min: 0 })("-0.001").value.rejected();
+      expect(expected).toBe("Too Low");
+    });
+
+    it('("+0.001").value.resolved() should return 0.001', () => {
+      const expected = parser({ min: 0 })("+0.001").value.resolved();
+      expect(expected).toBe(0.001);
+    });
+
+    it('(0).value.resolved() should return 0', () => {
+      const expected = parser({ min: 0 })(0).value.resolved();
+      expect(expected).toBe(0);
+    });
   });
 
-  it('()("xyz").value() should return "Invalid"', () => {
-    const expected = parser()('xyz').value();
-    expect(expected).toBe('Invalid');
+  describe('({ max: 0 })', () => {
+    it('("-0.001").value.resolved() should return -0.001', () => {
+      const expected = parser({ max: 0 })("-0.001").value.resolved();
+      expect(expected).toBe(-0.001);
+    });
+
+    it('("+0.001").value.rejected() should return "Too High"', () => {
+      const expected = parser({ max: 0 })("+0.001").value.rejected();
+      expect(expected).toBe('Too High');
+    });
+
+    it('(0).value.resolved() should return 0', () => {
+      const expected = parser({ max: 0 })(0).value.resolved();
+      expect(expected).toBe(0);
+    });
+  });
+
+  describe('({ required: true, min: 10, max: 15 })', () => {
+    it('().value.rejected() should return "Required"', () => {
+      const expected = parser({ required: true, min: 10, max: 15 })().value.rejected();
+      expect(expected).toBe("Required");
+    });
+
+    it('("?9.876?").value.rejected() should return "Too Low"', () => {
+      const expected = parser({ required: true, min: 10, max: 15 })("?9.876?").value.rejected();
+      expect(expected).toBe("Too Low");
+    });
+
+    it('("?15.341?").value.rejected() should return "Too High"', () => {
+      const expected = parser({ required: true, min: 10, max: 15 })("?15.341?").value.rejected();
+      expect(expected).toBe("Too High");
+    });
+
+    it('("??").value.rejected() should return "Invalid"', () => {
+      const expected = parser({ required: true, min: 10, max: 15 })("??").value.rejected();
+      expect(expected).toBe("Invalid");
+    });
+
+    it('("?12.752?").value.resolved() should return 12.752', () => {
+      const expected = parser({ required: true, min: 10, max: 15 })("?12.752?").value.resolved();
+      expect(expected).toBe(12.752);
+    });
   });
 });
